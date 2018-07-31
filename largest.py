efa839cfc6216075b78cf41a08a4ea2fb915524a
venkatasravanthi2 <41746796+venkatasravanthi2@users.noreply.github.com>
2018-07-31 15:21:52 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,7 @@
-num1=1
-num2=2
-num3=3
-if(num1>=num2) and (num1>=num3):
-    print(num1)
-elif(num2>=num1) and (num2>=num3):
-    print(num2)
+s,b,k=input().split()
+if(s>b) and (s>k):
+    print(s)
+elif(b>k):
+    print(b)
 else:
-    print(num3)
+    print(k)
